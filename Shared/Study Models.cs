11c75b440ce58c5a64709846dd2ffc4a3b5d7679
scanhammman <stevecanham@outlook.com>
2023-11-18 00:21:20 +0000

    Minor Tweaks

--- Shared/Study Models.cs
+++ Shared/Study Models.cs
@@ -32,65 +32,11 @@ public class StudySummary
 }
 
 
-/*
-public class DisplayStudy
-{
-    public int study_id { get; set; }
-    public string study_name { get; set; } = null!;
-    public string short_title { get; set; } = null!;
-    public string description { get; set; } = null!;
-    public string dss { get; set; } = null!;
-    public string start_date { get; set; } = null!;
-    public string type_name { get; set; } = null!;
-    public string status_name { get; set; } = null!;
-    public string gender_elig { get; set; } = null!;
-    public string min_age { get; set; } = null!;
-    public string max_age { get; set; } = null!;
-    public string feature_list { get; set; } = null!;
-    public string has_objects { get; set; } = null!;
-    public string country_list { get; set; } = null!;
-    public string condition_list { get; set; } = null!;
-    public string provenance { get; set; } = null!;
-    public List<ObjectSummary>? objects { get; set; }
-
-}
-*/
-/*
-public class StudyDetails
-{
-    int Id { get; set; }
-    string? Name { get; set; }
-    string? Description { get; set; }
-    string? DataSharingStatement { get; set; }
-    int TypeId { get; set; }
-    int StatusId { get; set; }
-    int StartYear { get; set; }
-    int StartMonth { get; set; }
-    string? MinAge { get; set; }
-    string? MaxAge { get; set; }
-    string? GenderEligibility { get; set; }
-    string? Provenance { get; set; }
-
-    List<string>? StudyIdentifiers { get; set; }
-    List<string>? Titles { get; set; }
-    List<string>? Conditions { get; set; }
-    List<string>? Topics { get; set; }
-    List<string>? Features { get; set; }
-    List<string>? Countries { get; set; }
-    List<string>? StudyPeople { get; set; }
-    List<string>? StudyOrganisations { get; set; }
-    List<string>? StudyRelationships { get; set; }
-    List<ObjectSummary>? ObjectDetails { get; set; }
-
-    // Inclusion / exclusion criteria - available as button
-}
-
-*/
-
 public class IECLine
 {
 
 }
+
 
 public class JSONFullStudyData
 {
@@ -131,64 +77,6 @@ public class JSONFullStudy
     public List<int>? linked_data_objects { get; set; }
 }
 
-/*
-[Table("search.new_studies")]
-public class StudyToSearchRecord
-{
-    public int study_id { get; set; }
-    public string? study_name { get; set; }
-    public int? start_year { get; set; }
-    public int? start_month { get; set; }
-    public int? type_id { get; set; }
-    public int? status_id { get; set; }
-    public int? phase_id { get; set; }
-    public int? alloc_id { get; set; }
-    public string? has_objects { get; set; }
-
-    public StudyToSearchRecord()
-    { }
-
-    public StudyToSearchRecord(JSONSSearchResStudy srs)
-    {
-        study_id = srs.study_id;
-        study_name = srs.study_name;
-        start_year = srs.start_year;
-        start_month = srs.start_month;
-        type_id = srs.type_id;
-        status_id = srs.status_id;
-        phase_id = srs.phase_id;
-        alloc_id = srs.alloc_id;
-        has_objects = srs.has_objects;
-    }
-}
-
-
-public class JSONSSearchResStudy
-{
-    public int study_id { get; set; }
-    public string? study_name { get; set; }
-    public string? description { get; set; }
-    public string? dss { get; set; }
-    public int? start_year { get; set; }
-    public int? start_month { get; set; }
-    public int? type_id { get; set; }
-    public string? type_name { get; set; }
-    public int? status_id { get; set; }
-    public string? status_name { get; set; }
-    public string? gender_elig { get; set; }
-    public string? min_age { get; set; }
-    public string? max_age { get; set; }
-    public int? phase_id { get; set; }
-    public int? alloc_id { get; set; }
-    public string? feature_list { get; set; }
-    public string? country_list { get; set; }
-    public string? condition_list { get; set; }
-    public string? has_objects { get; set; }
-    public List<JSONSearchResObject>? objects { get; set; }
-    public string? provenance { get; set; }
-
-}
-*/
 
 public class age_param
 {
